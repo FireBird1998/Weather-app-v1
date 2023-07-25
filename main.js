800c6d6c409aa7d0ee5980294299fbeeeba62c68
FireBird1998 <www.das5471@gmail.com>
2023-07-25 05:44:48 +0530

    js is starting to take shape

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import "./style.css"
+import { getWeather } from "./weather"
+
+getWeather(22.6547, 88.4467, Intl.DateTimeFormat().resolvedOptions().timeZone)
+    .then(({ current, daily, hourly }) => renderWeather(current, daily, hourly))
+    .catch(e => {
+        console.error(e);
+        alert("error getting weather." + e.message);
+    });
+
+
+
+function renderWeather(current, daily, hourly){
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+
+
+function renderCurrentWeather(current){
+    document.querySelector("[data-current-temp]").textContent = current.currentTemp
+}
