67050b93ea69b3dc4d462513399999199dce0000
FireBird1998 <www.das5471@gmail.com>
2023-07-27 02:43:23 +0530

    js is starting to take shapw part 2

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./style.css"
 import { getWeather } from "./weather"
+import { ICON_MAP } from "./iconMap";
 
 getWeather(22.6547, 88.4467, Intl.DateTimeFormat().resolvedOptions().timeZone)
     .then(({ current, daily, hourly }) => renderWeather(current, daily, hourly))
@@ -17,8 +18,23 @@ function renderWeather(current, daily, hourly){
     document.body.classList.remove("blurred")
 }
 
+function setValue(selector, value, {parent = document} = {}){
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
 
+function getIconUrl(iconCode){
+    return `public/icons/${ICON_MAP.get(iconCode)}.svg`
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
 
 function renderCurrentWeather(current){
-    document.querySelector("[data-current-temp]").textContent = current.currentTemp
+    currentIcon.src = getIconUrl(current.iconCode);
+    setValue("current-temp", current.currentTemp);
+    setValue("current-high", current.highTemp);
+    setValue("current-low", current.lowTemp);
+    setValue("current-fl-high", current.highFeelsLike);
+    setValue("current-fl-low", current.lowFeelsLike);
+    setValue("current-wind", current.windSpeed);
+    setValue("current-precip", current.precip);
 }
